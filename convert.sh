b1e65c2ffa2388424799b1d6c19a704c17d2dcb8
Plamen Ivanov <paco.iwanow@gmail.com>
2023-01-02 16:46:31 +0200

    update script

--- convert.sh
+++ convert.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+source .venv/bin/activate
+
 jupyter nbconvert "$1"/notebook.ipynb --to markdown --output README
 
 git add .
 git commit -m 'initial commit'
 git push
+
+deactivate
